2a2ba9a5907ee4d5f8b40bf224b3c8d8ce88d971
mrou-github <1593692219@qq.com>
2022-09-27 10:21:47 +0800

    路由命名

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -5,9 +5,7 @@
   <link rel="stylesheet" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-
 @include('layouts._header')
-
 <div class="container">
   <div class="offset-md-1 col-md-10">
     @yield('content')
